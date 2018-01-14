9b528ca7e547748403debdf38e6b2f30672e41a3
derek <d.frand@yahoo.com>
2018-01-13 23:45:28 -0600

    added materialize framework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,43 @@
 
-//__________________________________SEARCH_FUNCTION______________________________
+// ----------------------Needed for the Search form------------------------
+  $(document).ready(function() {
+// Initializes drop-down menus
+	$('select').material_select();
+// Initializes date menu
+      $('.datepicker').pickadate({
+          selectMonths: true, // Creates a dropdown to control month
+          selectYears: 15, // Creates a dropdown of 15 years to control year,
+          today: 'Today',
+          clear: 'Clear',
+          close: 'Ok',
+          closeOnSelect: false // Close upon selecting a date,
+      });
+  });
+  // ------------------------------End of Search form------------------------
 
-//_____________________________SEARCH BOX VARIABLES______________________________
-//takes input value from the search term box
-var searchTerm = $("#search-box").input().val().trim();
-//takes input value from the search dropdown box's location choice
-var searchLocation = $("#search-dropdown-location").input().val();
-//takes input value from the search dropdown box's date choice
-var searchDate = $("#search-dropdown-date").input().val();
-//takes input value from the search dropdown box's category choice
-var searchCategory = $("#search-dropdown-category").input().val();
-//makes our search button into a variale
-var searchBtn = $("#search-button");
-//______________________________________________________________________________
 
-var searchQuery;
-//need to put together search inputs and output the whole search input as "searchQuery."
+  //__________________________________SEARCH_FUNCTION______________________________
 
-//need function that takes in the whole search input and returns the search results
+  //_____________________________SEARCH BOX VARIABLES______________________________
+  //takes input value from the search term box
+  var searchTerm = $("#search-box").input().val().trim();
+  //takes input value from the search dropdown box's location choice
+  var searchLocation = $("#search-dropdown-location").input().val();
+  //takes input value from the search dropdown box's date choice
+  var searchDate = $("#search-dropdown-date").input().val();
+  //takes input value from the search dropdown box's category choice
+  var searchCategory = $("#search-dropdown-category").input().val();
+  //makes our search button into a variale
+  var searchBtn = $("#search-button");
+  //______________________________________________________________________________
 
-//need function that takes search results and outputs "results"(pictures+links) appropriately. would also get rid of the category images, leaving only the page header, search box, and results to see.
+  var searchQuery;
+  //need to put together search inputs and output the whole search input as "searchQuery."
 
-//Notes: search function is dependent on chosen date range, which must be accounted for through the API somehow
+  //need function that takes in the whole search input and returns the search results
 
-//if user presses logo, returns to page with default appearance with categories, etc.
+  //need function that takes search results and outputs "results"(pictures+links) appropriately. would also get rid of the category images, leaving only the page header, search box, and results to see.
+
+  //Notes: search function is dependent on chosen date range, which must be accounted for through the API somehow
+
+  //if user presses logo, returns to page with default appearance with categories, etc.
