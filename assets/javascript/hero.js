2eb56f3bce9a36de462f670263df3d0be1fec75d
Derek <d.frand@yahoo.com>
2018-01-23 09:30:04 -0600

    Added new respopnsive cards

---
+++ assets/javascript/hero.js
@@ -0,0 +1,13 @@
+
+//_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _Logo Animation_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+
+$(function() {
+  $("#transform").typed({
+    strings: ["LIFE", "ADVENTURE", "THE TWIN CITIES"],
+    typeSpeed: 50,
+    backSpeed: 10,
+    backDelay: 2000,
+    showCursor: false,
+    loop: false
+  });
+});
